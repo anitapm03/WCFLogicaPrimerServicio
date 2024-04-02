fb07099f23e5d13e495a0f7ba9fc644dc32b8fdf
anitapm03 <apereiramolinero@gmail.com>
2024-04-02 13:23:40 +0200

    Add project files.

---
+++ WCFLogicaPrimerServicio/IMetodosVariosContract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFLogicaPrimerServicio
+{
+    [ServiceContract]
+    internal interface IMetodosVariosContract
+    {
+        [OperationContract]
+        string GetSaludo(string nombre);
+
+        [OperationContract]
+        int GetNumeroDoble(int numero);
+
+        string MetodoInvisible();
+    }
+}
---
+++ WCFLogicaPrimerServicio/MetodosVariosClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFLogicaPrimerServicio
+{
+    internal class MetodosVariosClass : IMetodosVariosContract
+    {
+        public int GetNumeroDoble(int numero)
+        {
+            return numero * 2;
+        }
+
+        public string GetSaludo(string nombre)
+        {
+            return "Mi primer WCF, bienvenido " + nombre;
+        }
+
+        public string MetodoInvisible()
+        {
+            return "nada que haer :(";
+        }
+    }
+}
